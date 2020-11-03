4870bb26a9ab3d3b9ee3413ace9d925871f699b4
purcidma <72094665+purcidma@users.noreply.github.com>
2020-11-03 08:55:03 -0600

    Initial code

---
+++ main.tf
@@ -0,0 +1,62 @@
+ provider "aci" {
+      # cisco-aci user name
+      username = "YOUR-USERNAME"
+      # cisco-aci password
+      password = "YOUR-PWD"
+      # cisco-aci url
+      url      = "YOUR-URL-OR-IP"
+      insecure = true
+    }
+
+resource "aci_tenant" "terra-tenant" {
+  name        = "terra-tenant"
+  description = "This tenant is created by terraform"
+}
+
+resource "aci_vrf" "vrf-terra" {
+    tenant_dn = aci_tenant.terra-tenant.id
+    name = "vrf-terra"
+}
+
+resource "aci_application_profile" "general-network" {
+    tenant_dn = aci_tenant.terra-tenant.id
+    name = "general-network"
+}
+
+resource "aci_bridge_domain" "bd4" {
+    tenant_dn = aci_tenant.terra-tenant.id
+    relation_fv_rs_ctx = aci_vrf.vrf-terra.id
+    name = "bd4"
+}
+
+resource "aci_subnet" "bd4_subnet" {
+    parent_dn = aci_bridge_domain.bd4.id
+    ip = "4.4.4.1/24"
+}
+
+data "aci_vmm_domain" "vds" {
+    provider_profile_dn = "uni/vmmp-VMware"
+    name = "DVS-Site22"
+}
+
+resource "aci_application_epg" "epg4" {
+    application_profile_dn = aci_application_profile.general-network.id
+    name = "epg4"
+    relation_fv_rs_bd = aci_bridge_domain.bd4.id
+}
+
+resource "null_resource" "delay" {
+  provisioner "local-exec" {
+    command = "sleep 5"
+  }
+
+  triggers = {
+    "epg4" = aci_application_epg.epg4.id
+  }
+}
+
+resource "aci_epg_to_domain" "example" {
+    application_epg_dn = aci_application_epg.epg4.id
+    tdn = data.aci_vmm_domain.vds.id
+}
+
---
+++ vcenter.tf
@@ -0,0 +1,82 @@
+provider "vsphere" {
+    user = "YOUT-USER"
+    password = "YOUR-PWD"
+    vsphere_server = "YOUR-URL-OR-IP"
+    allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "YOUR-DATACENTER-ON-VMWARE"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "YOUR-DATASTORE-ON-VMWARE"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = "YOUR-CLUSTER-ON-VMWARE"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  #name          = format ("%v|%v|%v", aci_tenant.terra-tenant.name, aci_application_profile.general-network.name, aci_application_epg.epg4.name)
+  depends_on = [null_resource.delay]
+  name = "terra-tenant|general-network|epg4"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+ 
+data "vsphere_virtual_machine" "template" {
+  name          = "YOUR-TEMPLATE-ON-VMWARE"
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 2048
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label = "disk0"
+    size  = 100
+  }
+
+
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "terraform-test"
+        domain    = "test.internal"
+      }
+
+      network_interface {
+        ipv4_address = "4.4.4.10"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "4.4.4.1"
+    }
+
+  }
+}
+
+##################
+# OUTPUT
+##################
+output "ip" {
+    value = "${vsphere_virtual_machine.vm.*.default_ip_address}"
+}
